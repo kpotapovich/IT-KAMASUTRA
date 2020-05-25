761a01e07b6a1f0127058424314e7836bab8766b
kpotapovich <kris.potapovich@gmail.com>
2020-05-25 21:18:36 +0300

    доб, библиотеку react-router-dom -save, сделала переключение страниц

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,28 +1,23 @@
 import React from "react";
 import s from './Navbar.module.css';
 
-// let s = {
-//     'nav' : 'Navbar_nav__2-CC',
-//     'item': 'Navbar_item__3sdU9',
-// };
-
 
 const Navbar = () => {
   return <nav className ={s.nav}>
         <div className={s.item}>
-           <a>Profile</a>
+           <a href="/profile">Profile</a>
         </div>
         <div className={`${s.item} ${s.active}`}>
-           <a>Messages</a>
+           <a href="/dialogs">Messages</a>
         </div>
         <div className={s.item}>
-           <a>News</a>
+           <a href="/news">News</a>
         </div>
         <div className={s.item}>
-           <a>Music</a>
+           <a href="/music">Music</a>
         </div>
         <div className={s.item}>
-           <a>Settings</a>
+           <a href="/settings">Settings</a>
         </div>
   </nav>
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
     return (
-    <div className={s.content}>
+    <div>
         <div>
             <img src='https://p.bigstockphoto.com/eIdTXLbqQilMs9xbjvcs_bigstock-Aerial-View-Of-Sandy-Beach-Wit-256330393.jpg'/>
         </div>
