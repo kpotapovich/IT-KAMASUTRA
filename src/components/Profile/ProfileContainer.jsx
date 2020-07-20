c364e14a0266691ef42850660fd7f910223cd7d5
kpotapovich <kris.potapovich@gmail.com>
2020-07-20 12:37:35 +0300

    добавили статус с наведением, и обновили create-react-app

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -35,7 +35,6 @@ let mapStateToProps = (state) => ({
 export default compose(
     connect(mapStateToProps, {getUserProfile}),
     withRouter,
-    withAuthRedirect
 )(ProfileContainer);
 
 
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import s from './ProfileInfo.module.css';
 import Preloader from "../../Common/Preloader/Preloder";
+import ProfileStatus from "./ProfileStatus";
+
 
 
 const ProfileInfo = (props) => {
@@ -9,16 +11,14 @@ const ProfileInfo = (props) => {
     }
     return (
         <div>
-            <div>
-                <img
-                    src='https://p.bigstockphoto.com/eIdTXLbqQilMs9xbjvcs_bigstock-Aerial-View-Of-Sandy-Beach-Wit-256330393.jpg'/>
-            </div>
+            {/*<div>*/}
+            {/*    <img*/}
+            {/*        src='https://p.bigstockphoto.com/eIdTXLbqQilMs9xbjvcs_bigstock-Aerial-View-Of-Sandy-Beach-Wit-256330393.jpg'/>*/}
+            {/*</div>*/}
               <div className={s.descriptionBlock}>
                  <img src={props.profile.photos.large}/>
-                       ava + description
-                  <div></div>
+                      <ProfileStatus status={"Hello my friends"}/>
               </div>
-
         </div>
     )
 };
